38af50736e7777cd9308312d889d61213f9d7132
joaquinkeonimenendez-ops <joaquinkeonimenendez@gmail.com>
2026-02-28 08:45:53 -0800

    Format help output as numbered section list

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ function commander(cmd) {
   }
   if (cmd === "help") {
     addLine(
-      "Supported commands: about, projects, writings, contact.",
+      "1. About<br>2.Projects<br>3.Writings<br>Contact",
       "color2",
       80,
     );
