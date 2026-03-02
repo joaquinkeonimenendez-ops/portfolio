eaf7974528f739aa0ee948aabcbc7b43acf3987d
joaquinkeonimenendez-ops <joaquinkeonimenendez@gmail.com>
2026-03-01 18:03:00 -0800

    Remove line break after Back in Magnum hint

--- js/main.js
+++ js/main.js
@@ -745,7 +745,7 @@ function ensureMagnumShowcase() {
   const backLabel = document.createElement("span");
   backLabel.className = "command";
   backLabel.innerHTML =
-    "<- Back<br>(Type <u>projects</u> to return to the list of projects)";
+    "<- Back (Type <u>projects</u> to return to the list of projects)";
   backHint.appendChild(backLabel);
   backHint.addEventListener("click", function (e) {
     e.preventDefault();
